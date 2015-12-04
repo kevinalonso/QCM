99d379fe6dd7cf39cda6819fbcc70ddf645446a7
kevin <k.alonso@iia-laval.fr>
2015-12-04 11:20:51 +0100

    Création des class "Admin"

---
+++ src/AppBundle/Admin/UserAnswerAdmin.php
@@ -0,0 +1,13 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+ 
+class UserAnswerAdmin extends Admin
+{
+	
+}
